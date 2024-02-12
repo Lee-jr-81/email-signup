a42360ecb50ef2d12f834ec4aa72539745a6efa7
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2024-02-12 18:43:23 +0000

    added the .js file and started form validation logic

---
+++ src/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Selecting elements
+//
+const inputBox = document.getElementById("userInput");
+const submitButton = document.getElementById("submitButton");
+const confirmButton = document.getElementById("confirmationButton");
+const form = document.getElementById("emailSignup");
+
+// Grab email function
+//
+// function grabEmail(e) {
+//   e.preventDefault();
+//   const email = inputBox.value;
+//   console.log(email);
+// }
+
+// form.addEventListener("submit", grabEmail);
