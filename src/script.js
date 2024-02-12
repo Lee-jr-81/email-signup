c994e1ceb8fbd51dde8c6b1becf7a18e779f4879
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2024-02-12 19:35:45 +0000

    finished the component all tested and working

--- src/script.js
+++ src/script.js
@@ -6,13 +6,71 @@ const inputBox = document.getElementById("userInput");
 const submitButton = document.getElementById("submitButton");
 const confirmButton = document.getElementById("confirmationButton");
 const form = document.getElementById("emailSignup");
+const emailBox = document.getElementById("userInput");
+const errorBox = document.getElementById("errorBox");
+const mainForm = document.getElementById("signupForm");
+const successBox = document.getElementById("successBox");
 
-// Grab email function
 //
-// function grabEmail(e) {
-//   e.preventDefault();
-//   const email = inputBox.value;
-//   console.log(email);
-// }
+// Main function
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-// form.addEventListener("submit", grabEmail);
+  validateInputs();
+});
+
+//
+// Validate input function
+function validateInputs() {
+  const emailValue = inputBox.value.trim();
+  if (emailValue === "" || emailValue === null) {
+    inputBlank();
+  } else if (!isValidEmail(emailValue)) {
+    setError();
+  } else {
+    inputSuccess();
+  }
+}
+
+//
+// Valid email check
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//
+// error function
+function setError() {
+  emailBox.classList.add("bg-red-300");
+  emailBox.classList.add("text-red-500");
+  emailBox.classList.remove("text-slate-700");
+  errorBox.classList.remove("hidden");
+}
+
+//
+// Blank function
+function inputBlank() {
+  emailBox.classList.add("bg-red-300");
+  emailBox.classList.add("text-red-500");
+  emailBox.classList.remove("text-slate-700");
+  errorBox.classList.remove("hidden");
+  errorBox.innertext = "input field can not be blank";
+}
+
+//
+// Success function
+function inputSuccess() {
+  mainForm.classList.remove("grid");
+  mainForm.classList.remove("sm:flex");
+  mainForm.classList.add("hidden");
+  successBox.classList.remove("hidden");
+}
+
+confirmButton.addEventListener("click", function () {
+  mainForm.classList.remove("hidden");
+  mainForm.classList.add("grid");
+  mainForm.classList.add("sm:flex");
+  successBox.classList.add("hidden");
+});
